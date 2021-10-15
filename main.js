8e603c1e256cb64ccac1d5e93c32ca78894238b4
Taft Miller <taftmiller@Tafts-MacBook-Pro.local>
2021-10-14 21:33:04 -0600

    Third Commit, added more JS

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 let bidArray = [];
 
-function addOneBid() {
+function addOneBid(event) {
+    if(event.type === "keydown" && event.keyCode !== 13) return;
     let bidderName = "Bidder One";
     let bidderBid = document.getElementById("bidderOne").value;
     bidArray.push({
@@ -8,9 +9,11 @@ function addOneBid() {
         bid: bidderBid,
     });
     document.getElementById("bidderOne").value = "";
+    localStorageSave();
 }
 
-function addTwoBid() {
+function addTwoBid(event) {
+    if(event.type === "keydown" && event.keyCode !== 13) return;
     let bidderName = "Bidder Two";
     let bidderBid = document.getElementById("bidderTwo").value;
     bidArray.push({
@@ -18,4 +21,15 @@ function addTwoBid() {
         bid: bidderBid,
     });
     document.getElementById("bidderTwo").value = "";
+    localStorageSave();
 }
+
+function localStorageSave() {
+    localStorage.setItem("bidsArray", JSON.stringify(bidArray));
+}
+
+function localStorageBringBack() {
+    JSON.parse(localStorage.getItem("bidsArray"));
+}
+
+localStorageBringBack();
